218e2bc9d076c5b65f43f7ea0676199244ef37d0
GOMEL\Maksim_Lioshyn <Maksim_Lioshyn@epam.com>
2018-11-09 15:38:46 +0300

    Add empty progect

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { CartComponent } from './components/cart/cart.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -4,18 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CartComponent } from './components/cart/cart.component';
-import { CartListComponent } from './components/cart/cart-list/cart-list.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductListComponent } from './components/product/product-list/product-list.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    CartComponent,
-    CartListComponent,
-    ProductComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
