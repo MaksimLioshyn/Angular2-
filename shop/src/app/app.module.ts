8a1ca2214f5f95f0f80313a2cfff7410b53829c6
GOMEL\Maksim_Lioshyn <Maksim_Lioshyn@epam.com>
2018-11-16 02:24:33 +0300

    Add structure

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartListComponent } from './cart-list/cart-list.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CarditemComponent } from './carditem/carditem.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ProductComponent,
+    ProductListComponent,
+    CartListComponent,
+    CardListComponent,
+    CarditemComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
