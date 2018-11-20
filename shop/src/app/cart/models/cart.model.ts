4dae30d51877045c33df7b2d7abb8e42bf73ef0a
GOMEL\Maksim_Lioshyn <Maksim_Lioshyn@epam.com>
2018-11-20 18:04:24 +0300

    Add models

--- shop/src/app/cart/models/cart.model.ts
+++ shop/src/app/cart/models/cart.model.ts
@@ -1,2 +1,15 @@
-export class CartModel {
+export interface ICart {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export class Cart implements ICart {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number,
+    public quantity: number
+  ) {}
 }
